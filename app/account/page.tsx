3e1faf1ceab66fc5c120ac1a55568459b8ba26a4
Chris Ware <c.p.ware@hotmail.com>
2024-08-12 18:06:15 -0700

    Account Page updates

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -16,13 +16,31 @@ const options = [
   },
   {
     option: "Update My Account",
-    desc: "Update your username, email and password",
+    desc: "Update your username, email, and password.",
     href: "/me/update",
+  },
+  {
+    option: "Delete My Account",
+    desc: "Delete your account altogether.",
+    href: "/me/update",
+  },
+  {
+    option: "Admin Dashboard",
+    desc: "View Sales dashboard, update property details, manage bookings, view users, and verify reviews.",
+    href: "/admin/dashboard",
+    adminOnly: true, 
   },
 ];
 
 const AccountPage = () => {
   const { user } = useAppSelector((state) => state.auth);
+
+  // Filter options based on the user's role
+  const filteredOptions = options.filter(
+    (option) =>
+      !option.adminOnly || (option.adminOnly && user?.role === "admin")
+  );
+
   return (
     <main className={styles.parent}>
       <Nav color='olive' barColor='oliveBar' />
@@ -31,27 +49,16 @@ const AccountPage = () => {
           {user ? (
             <>
               <div className={styles.top}>
-                <h1 className={styles.heading}>
-                  Hi, {user?.name}
-                </h1>
-                <h2>What would you like to do today?</h2>
+                <h1 className={styles.heading}>Hi, {user?.name}</h1>
+                <h2 className={styles.heading2}>What would you like to do today?</h2>
               </div>
               <div className={styles.bottom}>
-                {options.map((x, index) => (
+                {filteredOptions.map((x, index) => (
                   <Link href={x.href} key={index} className={styles.box}>
                     <h3 className={styles.option}>{x.option}</h3>
                     <p className={styles.desc}>{x.desc}</p>
                   </Link>
                 ))}
-                {user?.role === "admin" && (
-                  <Link href='/admin/dashboard' className={styles.box}>
-                    <h3 className={styles.option}>Admin Dashboard</h3>
-                    <p className={styles.desc}>
-                      View Sales dashboard, update property details, manage
-                      bookings, view usuers, and verify reviews.
-                    </p>
-                  </Link>
-                )}
               </div>
             </>
           ) : (
@@ -70,4 +77,5 @@ const AccountPage = () => {
     </main>
   );
 };
+
 export default AccountPage;
